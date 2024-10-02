a597f9108273620f3d1702229303ce8bd903d1de
Sneyder Orellana <forsakenpenumbra@gmail.com>
2024-10-01 23:05:43 -0600

    No se pudooo (adapter)

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Singleton
 class Contador{
     constructor(){
         if(Contador.instance){
@@ -18,4 +19,41 @@ function actualizarClick(){
     const contador = new Contador();
     contador.añadirClick();
     document.getElementById("contador").textContent= contador.contarClick();
+}
+
+//TEACHEEER, no pude implementar adapter
+//Nuevo Click
+class NuevoClick {
+    constructor() {
+        this.click=0
+    }
+
+    incrementar() {
+        this.click++
+    }
+
+    actualizar() {
+        return this.click;
+    }
+}
+
+//Adapter
+class ContadorAdapter {
+    constructor(nuevo) {
+        this.nuevo = nuevo;
+    }
+
+    añadirClick() {
+        this.nuevo.incrementar();
+    }
+    contarClick() {
+        this.nuevo.actualizar();
+    }
+}
+const nuevo = new NuevoClick();
+
+function clickNuevo(){
+    const adaptador = new ContadorAdapter(nuevo);
+    adaptador.añadirClick();
+    document.getElementById("contador").textContent=adaptador.contarClick();
 }
